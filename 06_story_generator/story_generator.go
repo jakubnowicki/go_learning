bd75d1de8ff493bf8a62ca322d3399dcf1891e9e
Jakub Nowicki <kuba@appsilon.com>
2021-01-23 19:31:34 +0100

    Base story generator.

---
+++ 06_story_generator/story_generator.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
+
+var dictionary [][]string
+
+func getRandomElement(choices []string) string {
+	rand.Seed(time.Now().UnixNano())
+	randomIndex := rand.Intn(len(choices))
+	return choices[randomIndex]
+}
+
+func main() {
+	file, err := os.Open("story_parts.txt")
+
+	if err != nil {
+		log.Fatal("failed to open")
+	}
+
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	var text [][]string
+
+	for scanner.Scan() {
+		tmpText := strings.Split(scanner.Text(), ";")
+		text = append(text, tmpText)
+	}
+
+	file.Close()
+
+	var phrase string
+
+	for _, line := range text {
+		phrase = phrase + getRandomElement(line)
+	}
+	fmt.Println(phrase)
+}
